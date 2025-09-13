aefd2194f31daac9bd2754c87209ba315008c7be
Gabriel Nicolosi <gabianp@live.com.ar>
2025-09-13 19:14:14 -0300

    update baseURL

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/v1/api",
+  baseURL: "https://car-service-data-u0r9.onrender.com/v1/api",
   timeout: 5000,
   headers: {
     "Application-Type": "applicaton/json",
