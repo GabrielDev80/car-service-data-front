a05ed36871ddf996c35c7184d6c63670756fd7ab
Gabriel Nicolosi <gabianp@live.com.ar>
2025-10-25 00:35:50 -0300

    fix axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const baseURL = import.meta?.env?.VITE_API_URL;
+const baseURL = import.meta.env?.VITE_API_URL ?? "http://localhost:8080/v1/api";
+console.log("VITE_API_URL (import.meta.env):", import.meta.env?.VITE_API_URL);
 console.log("baseURL: ", baseURL);
+
 const api = axios.create({
   baseURL,
   timeout: 5000,
   headers: {
-    "Application-Type": "applicaton/json",
+    "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
