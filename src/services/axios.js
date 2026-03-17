2a1e45b0a0671df4a410aab7b2f3abafcb1f1893
Gabriel Nicolosi <gabianp@live.com.ar>
2026-03-17 20:43:21 -0300

    Axios configuration updated

--- src/services/axios.js
+++ src/services/axios.js
@@ -13,4 +13,18 @@ const api = axios.create({
   },
 });
 
+// Request interceptor to add the token to headers
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default api;
