c62394081a4096d4d5d186d6fa5d9b2e3d62e38b
Gabriel Nicolosi <gabianp@live.com.ar>
2026-04-02 21:47:58 -0300

    formatDateDDMMYYYY function in format.utils fixed

--- src/utils/formats.utils.js
+++ src/utils/formats.utils.js
@@ -44,8 +44,10 @@ export const formatDateDDMMYYYY = (dateString) => {
   if (!dateString) return "";
   const date = new Date(dateString);
   if (isNaN(date.getTime())) return "";
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+
+  const day = String(date.getUTCDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const year = date.getUTCFullYear();
+
   return `${day}-${month}-${year}`;
 };
